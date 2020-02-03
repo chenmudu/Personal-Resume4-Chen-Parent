2ea423f8e7ff0c8865c16e8512fe5608d4f21179
陈晨 <chenmudu@gmail.com>
2020-02-03 23:57:26 +0800

    add consistent hash (copy server ip put virtual node into ring container)

---
+++ 1-Algorithm-Brushing/src/main/java/org/chen/lb/iConsistentHashBalance/ConsistentHashGetServer.java
@@ -0,0 +1,29 @@
+package org.chen.lb.iConsistentHashBalance;
+
+import org.chen.lb.LoadBalanceIService;
+
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2019 chenmudu (陈晨)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * @Author chenchen6
+ * @Date: 2020/2/3 15:00
+ * @Description:
+ */
+public class ConsistentHashGetServer extends LoadBalanceIService {
+    @Override
+    public String getServerIp() {
+        return null;
+    }
+}
